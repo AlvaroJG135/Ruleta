01d87d3316e514e3539814a7f21541243f903213
AlvaroJG135 <alvarojimenez135@gmail.com>
2024-10-25 14:23:20 +0200

    Modificaciones Clase Menu y NumeroAleatorio

--- Menu.java
+++ Menu.java
@@ -1,43 +1,95 @@
-
 import java.util.Scanner;
-
 public class Menu {
     public static final String ROJO = "\u001B[31m";
-    public static final String RESET = "\u001B[0m"; // Resetea el color
+    public static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String opcion;
+        String color;
+        String parImpar;
+        int mitad;
+        int fila;
+        int docena;
+        boolean salir=false;
 
-        System.out.println();
-        System.out.println(ROJO+"3"+RESET+" 6 "+ROJO+"9 12"+RESET+" 15 "+ROJO+"18 21"+RESET+" 24 "+ROJO+"27 30"+RESET+ " 33 "+ROJO+"36"+RESET +"\n"+
-                            "2 "+ROJO+"5"+RESET+" 8 11 "+ROJO+"14"+RESET+" 17 20 "+ROJO+"23"+RESET+" 26 29 "+ROJO+"32"+RESET+ " 35"+"\n"+
-                            ROJO+"1"+RESET+" 4 "+ROJO+"7"+RESET+" 10 13 "+ROJO+"16 19"+RESET+" 22 "+ROJO+"25"+RESET+ " 28 31 "+ROJO+"34"+RESET);
-        System.out.println();
-        System.out.println("|1| Números");
-        System.out.println("|2| Color");
-        System.out.println("|3| Par - Impar");
-        System.out.println("|4| 1º Mitad - 2º Mitad");
-        System.out.println("|5| 1º Fila - 2º Fila - 3º Fila");
-        System.out.println("|X| Dejar de Jugar");
-        System.out.println();
+        do {
+            NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+            System.out.println();
+            System.out.println(ROJO+"3"+RESET+" 6 "+ROJO+"9 12"+RESET+" 15 "+ROJO+"18 21"+RESET+" 24 "+ROJO+"27 30"+RESET+ " 33 "+ROJO+"36"+RESET +"\n"+
+                                "2 "+ROJO+"5"+RESET+" 8 11 "+ROJO+"14"+RESET+" 17 20 "+ROJO+"23"+RESET+" 26 29 "+ROJO+"32"+RESET+ " 35"+"\n"+
+                                ROJO+"1"+RESET+" 4 "+ROJO+"7"+RESET+" 10 13 "+ROJO+"16 19"+RESET+" 22 "+ROJO+"25"+RESET+ " 28 31 "+ROJO+"34"+RESET);
+            System.out.println();
+            System.out.println("|1| Números");
+            System.out.println("|2| Color");
+            System.out.println("|3| Par - Impar");
+            System.out.println("|4| 1º Mitad - 2º Mitad");
+            System.out.println("|5| 1º Fila - 2º Fila - 3º Fila");
+            System.out.println("|6| 1º Docena - 2º Docena - 3º Docena");
+            System.out.println("|X| Dejar de Jugar");
+            System.out.println();
 
-        opcion = sc.next().toUpperCase();
-        switch (opcion) {
-            case "1":
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            case "4":
-                break;
-            case "5":
-                break;
-            case "X":
-                break;
-            default:
-                System.out.println("opcion no valida");
-        }
+            opcion = sc.next().toUpperCase();
+            switch (opcion) {
+                case "1":
+                    break;
+                case "2":
+                        sc.nextLine();
+                        System.out.println("Rojo - Negro");
+                        color = sc.nextLine().toUpperCase();
+                        if (color.equals(numeroAleatorio.colorDelNumero().toUpperCase())){
+                            System.out.println("has ganado "+numeroAleatorio.toString());
+                        }else{
+                            System.out.println("has perdido "+numeroAleatorio.toString());
+                        }
+                    break;
+                case "3":
+                        sc.nextLine();
+                        System.out.println("Par - Impar");
+                        parImpar = sc.nextLine().toUpperCase();
+                        if (parImpar.equals(numeroAleatorio.parImpar().toUpperCase())){
+                            System.out.println("has ganado "+numeroAleatorio.toString());
+                        }else{
+                            System.out.println("has perdido "+numeroAleatorio.toString());
+                        }
+                    break;
+                case "4":
+                        sc.nextLine();
+                        System.out.println("1º Mitad - 2º Mitad");
+                        mitad = sc.nextInt();
+                        if (mitad==numeroAleatorio.queMitadEs()){
+                            System.out.println("has ganado "+numeroAleatorio.toString());
+                        }else{
+                            System.out.println("has perdido "+numeroAleatorio.toString());
+                        }
+                    break;
+                case "5":
+                        sc.nextLine();
+                        System.out.println("1º Fila - 2º Fila - 3º Fila");
+                        fila = sc.nextInt();
+                        if (fila==numeroAleatorio.queFilaEs()){
+                            System.out.println("has ganado "+numeroAleatorio.toString());
+                        }else{
+                            System.out.println("has perdido "+numeroAleatorio.toString());
+                        }
+                    break;
+                case "6":
+                        sc.nextLine();
+                        System.out.println("1º Docena - 2º Docena");
+                        docena = sc.nextInt();
+                        if (docena==numeroAleatorio.queDocenaEs()){
+                            System.out.println("has ganado "+numeroAleatorio.toString());
+                        }else{
+                            System.out.println("has perdido "+numeroAleatorio.toString());
+                        }
+                    break;
+                case "X":
+                        salir=true;
+                        System.out.println("Saliendo");
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+            }
+        } while (salir!=true);
     }
 }
--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -5,6 +5,8 @@ public class NumeroAleatorio {
     int numero_ruleta;
     int[] numeros_negros = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
     int[] numeros_rojos = {2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
+    int[] fila3 = {3,6,9,12,15,18,21,24,27,30,33,36};
+    int[] fila2 = {2,5,8,11,14,17,20,23,26,29,32,35};
 
     public NumeroAleatorio() {
         numero_ruleta=rnd.nextInt(5);
@@ -31,25 +33,35 @@ public class NumeroAleatorio {
         }
         return "Rojo";
     }
-    public String queMitadEs(){
+    public int queMitadEs(){
         if (numero_ruleta>=1 && numero_ruleta<=18) {
-            return "Primera mitad 1-18";
+            return 1;
         }
-        return "Segunda mitad 19-36";
+        return 2;
     }
-    public String queDocenaEs(){
-        if(numero_ruleta>=1 && numero_ruleta<=12){
-            return "Primera docena 1-12";
-        }else if(numero_ruleta>=13 && numero_ruleta<=24){
-            return "Segunda docena 13-24";
+    public int queFilaEs(){
+        for (int i = 0; i < fila3.length; i++) {
+            if(numero_ruleta==fila3[i]){
+                return 3;
+            }else if (numero_ruleta==fila2[i]) {
+                return 2;
+            }
         }
-        return "Tercera docena 25-36";
+        return 1;
+    }
+    public int queDocenaEs(){
+        if(numero_ruleta>=1 && numero_ruleta<=12){
+            return 1;
+        }else if(numero_ruleta>=13 && numero_ruleta<=24){
+            return 2;
+        }
+        return 3;
     }
     @Override
     public String toString(){
         if(numero_ruleta==0){
             return numero_ruleta+" "+colorDelNumero();
         }
-            return numero_ruleta+" "+colorDelNumero()+" "+parImpar()+" ("+queDocenaEs()+") ("+queMitadEs()+")";
+            return numero_ruleta+" "+colorDelNumero()+" "+parImpar()+" ("+queMitadEs()+"º mitad)"+" ("+queFilaEs()+"º fila)"+" ("+queDocenaEs()+"º docena)";
     } 
 }
